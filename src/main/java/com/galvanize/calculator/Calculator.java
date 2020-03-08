56ded8c13aabc67c4d9cf559abd3e68919f1473d
Zika Chea <jallsadou@gmail.com>
2020-03-07 19:18:40 -0600

    mocking the calculator class

--- src/main/java/com/galvanize/calculator/Calculator.java
+++ src/main/java/com/galvanize/calculator/Calculator.java
@@ -1,10 +1,17 @@
+
+
+
+
 package com.galvanize.calculator;
-
 class Calculator {
-    CloudService service;
-
+    // Private service to allow it to be mocked and changed
+    private CloudService service;
+    // Constructor to define service variable
+    public Calculator(CloudService service) {
+        this.service = service;
+    }
+    // perform function
     public int perform(int i, int j) throws Exception {
-
         return service.add(i, j) * i;
     }
 }
--- src/test/java/com/galvanize/calculator/CalculatorTest.java
+++ src/test/java/com/galvanize/calculator/CalculatorTest.java
@@ -1,28 +1,61 @@
+//package com.galvanize.calculator;
+//
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.extension.ExtendWith;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//import static org.mockito.Mockito.*;
+//
+//
+//import org.mockito.junit.jupiter.MockitoExtension;
+//
+//@ExtendWith(MockitoExtension.class)
+//class CalculatorTest {
+//    Calculator calculator = null;
+//
+//    // Set up calculator object
+//    @BeforeEach
+//    public void setUp() {
+//        calculator = new Calculator();
+//    }
+//
+//    @Test
+//    public void testAdd() throws Exception {
+//
+//        assertEquals(10, calculator.perform(2, 3));
+//    }
+//}
+
+
+
+
 package com.galvanize.calculator;
-
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
-
-
-import org.mockito.junit.jupiter.MockitoExtension;
-
+        import org.junit.jupiter.api.BeforeEach;
+        import org.junit.jupiter.api.Test;
+        import org.junit.jupiter.api.extension.ExtendWith;
+        import static org.junit.jupiter.api.Assertions.*;
+        import static org.mockito.Mockito.*;
+        import org.mockito.junit.jupiter.MockitoExtension;
 @ExtendWith(MockitoExtension.class)
 class CalculatorTest {
     Calculator calculator = null;
-
-    // Set up calculator object
-    @BeforeEach
-    public void setUp() {
-        calculator = new Calculator();
-    }
-
+    // Manual
     @Test
     public void testAdd() throws Exception {
-
-        assertEquals(10, calculator.perform(2, 3));
+        Calculator calculator = new Calculator(new CloudService() {
+            // Manual Mock of the Cloud Service so we can focus on implementation of service rather than service itself.
+            @Override
+            public int add(int i, int j) throws Exception {
+                return i + j;
+            }
+        });
+        assertEquals(calculator.perform(1,2),3);
+    }
+    @Test
+    public void testAddWithMockito() throws Exception {
+        calculator = mock(Calculator.class);
+        when(calculator.perform(1,2)).thenReturn(3);
+        assertEquals(3, calculator.perform(1, 2));
     }
 }
